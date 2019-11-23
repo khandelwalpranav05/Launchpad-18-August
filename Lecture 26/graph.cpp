3d8cfeb63f8846029f9065d25b117421a7e45ec3
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2019-11-23 15:20:34 +0530

    graph

--- Lecture 26/graph.cpp
+++ Lecture 26/graph.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <list>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
@@ -120,7 +121,170 @@ public:
 		cout<<endl;
 	}
 
-	
+	void journeyToMoonHelper(T node,unordered_map<T,bool> &visited,int &countrySize){
+		countrySize++;
+		visited[node] = true;
+
+		for(T neighbor:AdjList[node]){
+			if(!visited[neighbor]){
+				journeyToMoonHelper(neighbor,visited,countrySize);
+			}
+		}
+	}
+
+	int journeyToMoon(){
+
+		unordered_map<T,bool> visited;
+
+		int n = AdjList.size();
+
+		int total = n*(n-1)/2;
+
+		for(auto node:AdjList){
+			if(!visited[node.first]){
+				int countrySize = 0;
+				journeyToMoonHelper(node.first,visited,countrySize);
+				total -= countrySize*(countrySize-1)/2;
+			}
+		}
+
+		return total;
+	}
+
+	void bfsTopologicalSort(){
+
+		queue<T> q;
+		unordered_map<T,int> indegree;
+
+		for(auto node:AdjList){
+			indegree[node.first] = 0;
+		}
+
+		for(auto node:AdjList){
+			for(T vertex:node.second){
+				indegree[vertex]++;
+			}
+		}
+
+		for(auto node:AdjList){
+			if(indegree[node.first]==0){
+				q.push(node.first);
+			}
+		}
+
+		while(!q.empty()){
+
+			T node = q.front();
+			q.pop();
+
+			cout<<node<<" ";
+
+			for(T neighbor:AdjList[node]){
+				indegree[neighbor]--;
+
+				if(indegree[neighbor]==0){
+					q.push(neighbor);
+				}
+			}
+		}
+
+		cout<<endl;
+	}
+
+	bool isCyclic(T src){
+
+		queue<T> q;
+		unordered_map<T,bool> visited;
+		unordered_map<T,T> parent;
+
+
+		parent[src] = src;
+		q.push(src);
+		visited[src] = true;
+
+		while(!q.empty()){
+
+			T node = q.front();
+			q.pop();
+
+			for(T neighbor:AdjList[node]){
+
+				if(visited[neighbor] and parent[node]!=neighbor){
+					return true;
+
+				}else if(!visited[neighbor]){
+					q.push(neighbor);
+					visited[neighbor] = true;
+					parent[neighbor] = node;
+				}
+
+			}
+		}
+
+		return false;
+	}
+
+	void bfsShortestPath(T src){
+
+		queue<T> q;
+		unordered_map<T,int> dist;
+
+		for(auto node:AdjList){
+			dist[node.first] = INT_MAX;
+		}
+
+		dist[src] = 0;
+		q.push(src);
+
+		while(!q.empty()){
+
+			T node = q.front();
+			q.pop();
+
+			for(T neighbor:AdjList[node]){
+
+				if(dist[neighbor]==INT_MAX){
+					dist[neighbor] = dist[node] + 1;
+					q.push(neighbor);
+				}
+
+			}
+		}
+
+		for(auto node:dist){
+			cout<<"Node "<<node.first<<" is at a distance of "<<node.second<<endl;
+		}
+	}
+
+	int snakesAndLadder(T src,T dest){
+
+		queue<T> q;
+		unordered_map<T,int> dist;
+
+		for(auto node:AdjList){
+			dist[node.first] = INT_MAX;
+		}
+
+		dist[src] = 0;
+		q.push(src);
+
+		while(!q.empty()){
+
+			T node = q.front();
+			q.pop();
+
+			for(T neighbor:AdjList[node]){
+
+				if(dist[neighbor]==INT_MAX){
+					dist[neighbor] = dist[node] + 1;
+					q.push(neighbor);
+				}
+
+			}
+		}
+
+		return dist[dest];
+	}
 
 };
 
@@ -152,15 +316,75 @@ int main(){
 
 	// g.display();
 
-	Graph<string> g;
+	// Graph<string> g;
 
-	g.addEdge("Maths","Programming",false);
-	g.addEdge("English","Programming",false);
-	g.addEdge("Programming","Python",false);
-	g.addEdge("Programming","Java",false);
-	g.addEdge("Java","Web",false);
-	g.addEdge("Python","Web",false);
+	// g.addEdge("Maths","Programming",false);
+	// g.addEdge("English","Programming",false);
+	// g.addEdge("Programming","Python",false);
+	// g.addEdge("Programming","Java",false);
+	// g.addEdge("Java","Web",false);
+	// g.addEdge("Python","Web",false);
 
-	g.dfsTopologicalSort("Maths");
+	// // g.dfsTopologicalSort("Maths");
+	// g.bfsTopologicalSort();
+
+	// Graph<int> g;
+
+	// g.addEdge(1,2);
+	// g.addEdge(1,3);
+	// g.addEdge(1,4);
+	// g.addEdge(5,6);
+	// g.addEdge(6,7);
+
+	// cout<<g.journeyToMoon()<<endl;
+
+	// Graph<int> g;
+
+	// g.addEdge(1,2);
+	// g.addEdge(1,3);
+	// // g.addEdge(2,3);
+	// g.addEdge(3,4);
+	// g.addEdge(4,5);
+
+	// cout<<g.isCyclic(1)<<endl;
+
+	// Graph<int> g;
+
+	// g.addEdge(1,2);
+	// g.addEdge(1,3);
+	// g.addEdge(2,4);
+	// g.addEdge(3,5);
+	// g.addEdge(3,4);
+	// g.addEdge(5,6);
+	// g.addEdge(4,5);
+
+	// g.bfsShortestPath(1);
+
+	Graph<int> g;
+
+	int board[50] = {0};
+
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[17] = -13;
+	board[20] = -14;
+	board[24] = -8;
+	board[25] = -10;
+	board[32] = -2;
+	board[34] = -22;
+
+	for(int i=0;i<=36;i++){
+		for(int dice = 1;dice<=6;dice++){
+
+			int u = i;
+			int v = u + dice + board[u+dice];
+			g.addEdge(u,v,false);
+		}
+	}
+
+	cout<<g.snakesAndLadder(0,36)<<endl;
+
 	return 0;
 }
